ed560521d7e6e31380134bb613bcd0adc7c481ca
Oscar-G0m3z <113945924+Oscar-G0m3z@users.noreply.github.com>
2026-01-08 11:14:15 -0800

    s_matrix and matrix_gen

--- lab1/matrix_multiply/matrix_multiply.go
+++ lab1/matrix_multiply/matrix_multiply.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"math/rand/v2")
 
+func s_matrix(a [][]int, b [][]int) [][]int {
+}
+		
 func main() {
-	var a = [2][3]int{{1, 2, 3}, {4, 5, 6}};
-	var b = [3][2]int{{7, 8}, {9, 10}, {11, 12}};
+	fmt.Println(matrix_gen(10,10))
+	var a = [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	var b = [3][2]int{{7, 8}, {9, 10}, {11, 12}}
 }
 
-func matrixMultiply(a [][]int, b [][]int) [][]int {
+func matrix_gen(r int, c int) [][]int {
+	var i int
+	var j int
+	var matrix [][]int
+
+	for i:=0, i<r, i++{
+		for j:=0, j<c, j++{
+			matrix[i][j] = rand.Int(100)
+		}
+	}
+	return matrix
 }
