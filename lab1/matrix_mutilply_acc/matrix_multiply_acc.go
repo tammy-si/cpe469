97bdabd5f96b16866a9c902525301be9f9d187e3
tammy-si <sitammy6@gmail.com>
2026-01-10 09:11:43 -0800

    Accelerated should work just takes forever

--- lab1/matrix_mutilply_acc/matrix_multiply_acc.go
+++ lab1/matrix_mutilply_acc/matrix_multiply_acc.go
@@ -1,66 +1,83 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"math/rand/v2"
+	    "sync"
+		"time"
+)
 
 func main() {
-	// test for now
-	a := [][]int{
-		{1, 2, 3},
-		{4, 5, 6},
-	}
-	b := [][]int {
-		{7, 8},
-		{9, 10},
-		{11, 12},
-	}
-	
-	result := matrixMultiplyConcurrent(a, b)
-	fmt.Println("Resultant Matrix:")
-	for _, row := range result {
-		fmt.Println(row)
-	}
+	big_a := matrix_gen(10000, 10000);
+	big_b := matrix_gen(10000, 10000);
+
+	for _, goroutines := range []int {1, 2, 4, 8, 16, 32} {
+		start := time.Now()
+		concurrentResult := matrixMultiplyConcurrent(big_a, big_b, goroutines);
+		elasped := time.Since(start)
+		fmt.Println("Concurrent (%d gorountines) time: %v \n", goroutines, elasped);
+
+		var sum float32
+		for i := 0; i < len(concurrentResult); i++ {
+			sum += concurrentResult[i][i]
+		}
+		fmt.Printf("Concurrent (%d goroutines) time: %v, diagonal sum: %v\n", goroutines, elasped, sum)
+	}	
 }
 
-func matrixMultiplyConcurrent(a [][]int, b [][]int) [][]int {
+func matrixMultiplyConcurrent(a [][]float32, b [][]float32, numGoroutines int) [][]float32 {
 	n := len(a)
 	m := len(b[0])
 	p := len(b)
 
 	// Result matrix has the row count of a and column count of b
-	finalResult := make([][]int, n)
+	finalResult := make([][]float32, n)
 	for i := range finalResult {
-		finalResult[i] = make([]int, m)
+		finalResult[i] = make([]float32, m)
 	}
 
-	// Channel to collect results
-	type result struct {
-		row int
-		col int
-		val int
-	}
-	resultCh := make(chan result)
+	var wg sync.WaitGroup
+	wg.Add(numGoroutines);
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			// func (i, j int) defines an anonymous function that calculates the value for finalResult[i][j]
-			go func(i, j int) {
-				sum := 0
-				for k := 0; k < p; k++ {
-					sum += a[i][k] * b[k][j]
+	// Compute row ranges for each goroutine, rounds up
+	chunkSize := (n + numGoroutines - 1) / numGoroutines
+
+	for g := 0; g < numGoroutines; g++ {
+		startRow := g * chunkSize
+		endRow := startRow + chunkSize
+		if endRow > n {
+			endRow = n
+		}
+		
+		go func(start, end int) {
+			for i := start; i < end; i++ {
+				for j := 0; j < m; j++ {
+					var sum float32
+					for k := 0; k < p; k++ {
+						sum += a[i][k] * b[k][j]
+					}
+					finalResult[i][j] = sum
 				}
-				// Put results for the calculated cell into the channel
-				resultCh <- result{row: i, col: j, val: sum}
-			}(i, j)
-		}
+			}
+			wg.Done()
+		} (startRow, endRow)
 	}
 
-	// Collect results for every cell in the result matrix from the channel
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			res := <-resultCh
-			finalResult[res.row][res.col] = res.val
-		}
-	}
+	wg.Wait();
 
 	return finalResult
+}
+
+// taken from matrix_multipliy.go
+func matrix_gen(r int, c int) [][]float32 {
+	matrix := make([][]float32, r)
+	for i := range matrix {
+		matrix[i] = make([]float32, c)
+	}
+
+	for i:=0; i<r; i++{
+		for j:=0; j<c; j++{
+			matrix[i][j] = rand.Float32() * 100
+		}
+	}
+	return matrix
 }
